57bef8d26b2025de29e948973665247f9be6a921
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 01:28:16 +0200

    adding form to modal

--- components/modal/store-modal.tsx
+++ components/modal/store-modal.tsx
@@ -7,7 +7,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from 'zod';
-import { Form } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage,  } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
     name: z.string().min(2, {message: "Store name should be minimum 2 characters"})
@@ -39,10 +41,27 @@ export const StoreModal = () => {
                 <div className="space-y-4 py-2 pb-4">
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)}>
-
+                            <FormField control={form.control} name="name" render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Name</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isLoading}
+                                            placeholder="Store name"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+                            )} />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button disabled={isLoading} type="submit">Create</Button>
+                                <Button disabled={isLoading} type="button" variant={"destructive"}>Cancel</Button>
+                            </div>
                         </form>
                     </Form>
                 </div>
+                
             </div>
       </Modal>
     )
