e275ebb14808eb1a01b32fe73d6ba26319da1a32
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 22:40:16 +0200

    fetching store details and displaying

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,8 +1,17 @@
+import { db } from "@/lib/firebase"
+import { Store } from "@/types-db"
+import { doc, getDoc } from "firebase/firestore"
 
+interface DashboardOverviewProps{
+    params: {storeId: string}
+}
 
-const DashboardOverview = () => {
+const DashboardOverview = async ({ params }: DashboardOverviewProps) => {
+    const store = (
+        await getDoc(doc(db, "stores", params.storeId))
+    ).data() as Store
   return (
-    <div>DashboardOverview</div>
+      <div> {store.name} Overview</div>
   )
 }
 
