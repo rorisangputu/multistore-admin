f28d00728ec7a0c1e8eb2ee6b601ef323a90a4bc
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 21:25:56 +0200

    getting db snapshot of specific store

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,4 +1,6 @@
+import { db } from "@/lib/firebase";
 import { auth } from "@clerk/nextjs/server"
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { redirect } from "next/navigation";
 
 interface DashboardLayoutProps{
@@ -9,10 +11,19 @@ interface DashboardLayoutProps{
 
 const DashboardLayout = async ({ children, params }: DashboardLayoutProps) => {
     const { userId } = await auth();
-    
+
     if (!userId) {
         redirect("/sign-in")
     }
+
+    const storeSnap = await getDocs(
+        query(
+            collection(db, "stores"),
+            where("userId", "==", userId),
+            where("id", "==", params.storeId)
+        )
+    )
+
   return (
       <div>
           
