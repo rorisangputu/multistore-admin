69cd07915461f510fe3c4f8b5cdd490444736414
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 22:52:29 +0200

    styling navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,21 @@
+import { SignedIn, UserButton } from "@clerk/nextjs"
+
+
+const Navbar = () => {
+  return (
+      <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+              <p>This is store switcher</p>
+
+              {/* Routes */}
+
+              {/* User profile */}
+            <SignedIn>
+                <UserButton  afterSwitchSessionUrl="/"/>
+            </SignedIn>
+          </div>
+    </div>
+  )
+}
+
+export default Navbar
