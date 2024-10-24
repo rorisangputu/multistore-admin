d8b085aa1399a2c587aa7b23999749055a37f686
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 21:59:35 +0200

    fetching stores and redirecting to users store

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,7 @@
+import { db } from "@/lib/firebase";
+import { Store } from "@/types-db";
 import { auth } from "@clerk/nextjs/server"
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { redirect } from "next/navigation";
 
 interface SetupLayoutProps{
@@ -11,7 +14,22 @@ const SetupLayout = async ({ children }: SetupLayoutProps) => {
 
   if (!userId) {
         redirect("/sign-in")
-    }
+  }
+  
+  const storeSnap = await getDocs(
+    query(collection(db, "Stores"), where("userId", "==", userId))
+  )
+
+  let store = null as any;
+
+  storeSnap.forEach(doc => {
+    store = doc.data() as Store
+    return
+  })
+
+  if (store) {
+    redirect(`/${store?.id}`)
+  }
 
 
   return (
