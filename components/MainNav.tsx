540e6c03277524cce8f18fd300efc15b55d2dd55
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 23:15:29 +0200

    creating client nav

---
+++ components/MainNav.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation"
+
+const MainNav = ({ className, ...props }: React.AllHTMLAttributes<HTMLElement>) => {
+    const pathname = usePathname();
+    const params = useParams();
+
+    const routes = [
+        {
+            href: `/${params.storeId}/settings`,
+            label: "Settings",
+            active: `/${params.storeId}/settings`
+        }
+    ]
+
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6 pl-6")}>
+          {routes.map(route => (
+              <Link key={route.href} href={route.href} className={cn("text-sm font-medium transition-colors hover:text-primary", route.active ? "text-black dark:text-white" : "text-muted-foreground")}>
+              {route.label}
+              </Link>
+       ))}
+    </nav>
+  )
+}
+
+export default MainNav
