dea3575d0fccb50a1946e3331c3627ee5216bdcf
Rorisang Putu <rorisangputu@gmail.com>
2024-10-24 21:55:31 +0200

    getting userId

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,19 @@
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation";
 
 interface SetupLayoutProps{
     children: React.ReactNode
 }
 
-const SetupLayout = ({children} : SetupLayoutProps) => {
+const SetupLayout = async ({ children }: SetupLayoutProps) => {
+  
+  const { userId } = await auth();
+
+  if (!userId) {
+        redirect("/sign-in")
+    }
+
+
   return (
     <div>
          {children} 
