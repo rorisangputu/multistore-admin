c4e10d3128f4ecaa6a9521b5d87721491ff040c3
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 23:07:30 +0200

    creating props

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,25 +1,33 @@
-"use client"
+"use client";
 
 import Heading from "@/components/Heading";
 import { Button } from "@/components/ui/button";
+import { Billboards } from "@/types-db";
 import { Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation"
+import { useParams, useRouter } from "next/navigation";
 
-
-
-const BillboardClient = () => {
-    const params = useParams();
-    const router = useRouter();
-
-  return (
-      <div className="flex items-center justify-between">
-          <Heading title="Billboards (0)" description="Manage billboards for your store" />  
-          <Button onClick={() => router.push(`/${params.storeId}/billboards/create`)}>
-              <Plus className="h-4 w-4" />
-              Add New
-          </Button>
-    </div>
-  )
+interface BillboardClientProps {
+  data: Billboards[];
 }
 
-export default BillboardClient
+const BillboardClient = ({ data }: BillboardClientProps) => {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <div className="flex items-center justify-between">
+      <Heading
+        title="Billboards (0)"
+        description="Manage billboards for your store"
+      />
+      <Button
+        onClick={() => router.push(`/${params.storeId}/billboards/create`)}
+      >
+        <Plus className="h-4 w-4" />
+        Add New
+      </Button>
+    </div>
+  );
+};
+
+export default BillboardClient;
