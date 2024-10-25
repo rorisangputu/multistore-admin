9c32649b16e3c264b4e7539bcc6a89fb0b398bcb
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 16:30:01 +0200

    creating billboards page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Billboards = () => {
+  return (
+    <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Billboards
