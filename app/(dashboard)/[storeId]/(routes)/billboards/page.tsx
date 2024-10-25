ccfa696249c21e3918b898c974de8e535858a4ca
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 22:56:57 +0200

    fetching billboard data

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,14 +1,20 @@
-import BillboardClient from "./components/client"
+import { collection, doc, getDocs } from "firebase/firestore";
+import BillboardClient from "./components/client";
+import { db } from "@/lib/firebase";
+import { Billboards } from "@/types-db";
 
+const BillboardsIndex = async ({ params }: { params: { storeId: string } }) => {
+  const billboardsData = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "billboards"))
+  ).docs.map((doc) => doc.data()) as Billboards[];
 
-const Billboards = ({params}: {params: {storeId: string}}) => {
   return (
     <div className="flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">
-            <BillboardClient/>
-        </div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Billboards
+export default BillboardsIndex;
