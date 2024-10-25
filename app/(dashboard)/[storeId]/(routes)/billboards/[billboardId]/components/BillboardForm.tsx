29e9e2d9511e925554127806461fa1f67288c333
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 18:56:23 +0200

    uploading image and displaying uploaded image

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/BillboardForm.tsx
@@ -1,13 +1,20 @@
-"use client"
+"use client";
 
 import Heading from "@/components/Heading";
 import ImageUpload from "@/components/imageUpload";
 import AlertModal from "@/components/modal/alert-modal";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { Billboards } from "@/types-db"
+import { Billboards } from "@/types-db";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { Trash } from "lucide-react";
@@ -17,116 +24,124 @@ import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { z } from "zod";
 
-interface BillboardFormProps{
-    initialData : Billboards
+interface BillboardFormProps {
+  initialData: Billboards;
 }
 
 const formSchema = z.object({
-    label: z.string().min(1),
-    imageUrl: z.string().min(1)   
-})
+  label: z.string().min(1),
+  imageUrl: z.string().min(1),
+});
 
 const BillboardForm = ({ initialData }: BillboardFormProps) => {
-    
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: initialData,
-    });
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
 
-    const [isLoading, setIsLoading] = useState(false);
-    const [open, setOpen] = useState(false);
-    const params = useParams();
-    const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const params = useParams();
+  const router = useRouter();
 
-    const title = initialData ? "Edit Billboards" : "Create New Billboard";  
-    const desc = initialData ? "Edit your billboard" : "Add a billboard";
-    const toastMessage = initialData ? "Billboard Updated" : "Billboard Created!";
-    const action = initialData ? "Save Changes" : "Create Billboard";
+  const title = initialData ? "Edit Billboards" : "Create New Billboard";
+  const desc = initialData ? "Edit your billboard" : "Add a billboard";
+  const toastMessage = initialData ? "Billboard Updated" : "Billboard Created!";
+  const action = initialData ? "Save Changes" : "Create Billboard";
 
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    //console.log(data);
 
-    const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        //console.log(data);
-
-        try {
-            setIsLoading(true);
-            const response = await axios.patch(`/api/stores/${params.storeId}`, data);
-            toast.success(toastMessage);
-            router.refresh();
-        } catch (error) {
-            toast.error("Unable to update store name")
-        } finally {
-            setIsLoading(false);
-        }
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(`/api/stores/${params.storeId}`, data);
+      toast.success(toastMessage);
+      router.refresh();
+    } catch (error) {
+      toast.error("Unable to update store name");
+    } finally {
+      setIsLoading(false);
     }
-    const onDelete = async () => {
-        
+  };
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.delete(`/api/stores/${params.storeId}`);
 
-        try {
-            setIsLoading(true);
-            const response = await axios.delete(`/api/stores/${params.storeId}`);
-            
-            if (response) {
-                toast.success("Store removed");
-            router.refresh();
-            router.push("/");
-            }
-        } catch (error) {
-            toast.error("Unable to delete store")
-        } finally {
-            setIsLoading(false);
-        }
+      if (response) {
+        toast.success("Store removed");
+        router.refresh();
+        router.push("/");
+      }
+    } catch (error) {
+      toast.error("Unable to delete store");
+    } finally {
+      setIsLoading(false);
     }
-
+  };
 
   return (
-    <>  
-        <div className="flex items-center justify-center">
-            <Heading title={title} description={desc}/>
-            {initialData ? (<div className="bg-red-600 flex items-center px-5 gap-2 text-white py-2 ">
-                <Trash className="h-4 w-4 text-white" />
-                <AlertModal onConfirm={onDelete} loading={isLoading} />
-            </div>) : ""}
-        </div>
-        <Separator />
-        <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
-                    <FormField
-                        control={form.control} name="imageUrl" render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Image</FormLabel>
-                                <FormControl>
-                                    <ImageUpload
-                                      value={field.value ? [field.value] : []}
-                                      disabled={isLoading}
-                                      onChange={() => { }}
-                                      onRemove={() => { }}
-                                    />
-                                </FormControl>
-                            </FormItem>
-                        )}
+    <>
+      <div className="flex items-center justify-center">
+        <Heading title={title} description={desc} />
+        {initialData ? (
+          <div className="bg-red-600 flex items-center px-5 gap-2 text-white py-2 ">
+            <Trash className="h-4 w-4 text-white" />
+            <AlertModal onConfirm={onDelete} loading={isLoading} />
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <ImageUpload
+                    value={field.value ? [field.value] : []}
+                    disabled={isLoading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={() => field.onChange("")}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <div className="grid grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="label"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Billboard Name"
+                      {...field}
                     />
-                <div className="grid grid-cols-3 gap-8">
-                    <FormField control={form.control} name="label" render={({ field }) => (
-                        <FormItem>
-                            <FormLabel>Name</FormLabel>
-                            <FormControl>
-                                <Input
-                                    disabled={isLoading}
-                                    placeholder="Billboard Name"
-                                    {...field}
-                                />
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )} />      
-                </div>
-                <Button disabled={isLoading} type="submit">{action}</Button>
-            </form>
-        </Form>
-        
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={isLoading} type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
     </>
-  )
-}
+  );
+};
 
-
-export default BillboardForm
+export default BillboardForm;
--- components/imageUpload.tsx
+++ components/imageUpload.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { storage } from "@/lib/firebase";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { ImagePlus } from "lucide-react";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { PuffLoader } from "react-spinners";
 
 interface ImageUploadProps {
@@ -29,10 +33,52 @@ const ImageUpload = ({
     return null;
   }
 
+  const onUpload = async (e: any) => {
+    const file = e.target.files[0];
+    setIsLoading(true);
+
+    const UploadTask = uploadBytesResumable(
+      ref(storage, `Images/${Date.now()}-${file.name}`),
+      file,
+      { contentType: file.type }
+    );
+
+    UploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+      },
+      (error) => {
+        toast.error(error.message);
+      },
+      () => {
+        getDownloadURL(UploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at: ", downloadURL);
+          onChange(downloadURL);
+          setIsLoading(false);
+        });
+      }
+    );
+  };
+
   return (
     <div>
       {value && value.length > 0 ? (
-        <></>
+        <>
+          {value.map((url) => (
+            <div
+              className="relative w-52 h-52 round-md overflow-hidden"
+              key={url}
+            >
+              <Image
+                fill
+                className="object-cover"
+                alt="Billboard image"
+                src={url}
+              />
+            </div>
+          ))}
+        </>
       ) : (
         <div
           className="w-52 h-52 rounded-md overflow-hidden border border-dashed border-gray-200
@@ -53,7 +99,12 @@ const ImageUpload = ({
                   <ImagePlus className="h-4 w-4" />
                   <p>Image Upload</p>
                 </div>
-                <input type="file" accept="image/*" className="" />
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={onUpload}
+                  className="w-0 h-0"
+                />
               </label>
             </>
           )}
