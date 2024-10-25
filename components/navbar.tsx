c2d90b7b0feabd2e604b7f36cf8312e1dcfa03c1
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 09:18:51 +0200

    fetching store data

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,35 @@
 import { SignedIn, UserButton } from "@clerk/nextjs"
 import MainNav from "@/components/MainNav"
 import StoreSwitcher from "@/components/StoreSwitcher"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { collection, getDocs, query, where } from "firebase/firestore"
+import { db } from "@/lib/firebase"
+import { Store } from "@/types-db"
 
 
-const Navbar = () => {
+const Navbar = async () => {
+    const { userId } = await auth()
+    
+    if (!userId)
+    {
+        redirect('/');
+    }
+
+    const storeSnap = await getDocs(
+        query(collection(db, "stores"), where("userId", "==", userId))
+    );
+
+    let stores = [] as Store[];
+
+    storeSnap.forEach(doc => {
+        stores.push(doc.data() as Store)
+    })
+
   return (
       <div className="border-b">
           <div className="flex h-16 items-center px-4">
-              <StoreSwitcher items={[]} />
+              <StoreSwitcher items={stores} />
 
               {/* Routes */}
               <MainNav/>
