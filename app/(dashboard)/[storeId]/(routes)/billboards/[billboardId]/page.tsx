dd29e73d3f16e7839fca5519725e7a0be8864590
Rorisang Putu <rorisangputu@gmail.com>
2024-10-25 17:36:17 +0200

    fetching billboard data

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/firebase"
 import { Billboards } from "@/types-db"
 import { doc, getDoc } from "firebase/firestore"
+import BillboardForm from "./components/BillboardForm";
 
 
 const BillboardPage = async ({ params }: { params: { storeId: string, billboardId: string } }) => {
@@ -9,10 +10,15 @@ const BillboardPage = async ({ params }: { params: { storeId: string, billboardI
         await getDoc(doc(db, "stores", params.storeId, "billboards", params.billboardId))
     ).data() as Billboards;
 
-    
+
   return (
-    <div>BillboardPage</div>
+    <div className="flex-col">
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            <BillboardForm initialData={billboard} />
+        </div>
+    </div>
   )
 }
 
 export default BillboardPage
+ 
