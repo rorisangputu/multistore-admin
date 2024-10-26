c44e7392e1fbd016090b52bf042e8753a27f797e
Rorisang Putu <rorisangputu@gmail.com>
2024-10-26 19:21:46 +0200

    adding columns

--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -7,27 +7,73 @@ import CellAction from "./CellAction";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type SizeColumns = {
+export type ProductColumns = {
   id: string;
   name: string;
-  value: string;
+  price: number;
+  qty?: number;
+  images: { url: string }[];
+  isFeatured: boolean;
+  isArchived: boolean;
+  category: string;
+  size: string;
+  kitchen: string;
+  cuisine: string;
   createdAt: string;
 };
 
-export const Columns: ColumnDef<SizeColumns>[] = [
+export const Columns: ColumnDef<ProductColumns>[] = [
   {
     accessorKey: "name",
-    header: "Size",
-  },
-  {
-    accessorKey: "value",
     header: ({ column }) => {
       return (
         <Button
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Size
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "kitchen",
+    header: "Kitchen",
+  },
+  {
+    accessorKey: "cuisines",
+    header: "Cuisine",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Date
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
