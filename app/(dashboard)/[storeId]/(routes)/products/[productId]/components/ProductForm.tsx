284c6c5fe1c4c25b755ce81a5a514976058b8bf7
Rorisang Putu <rorisangputu@gmail.com>
2024-10-26 20:13:16 +0200

    adding fields

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/components/ProductForm.tsx
@@ -13,6 +13,13 @@ import {
 } from "@/components/ui/form";
 
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Separator } from "@/components/ui/separator";
 import { Categories, Cuisines, Kitchen, Product, Sizes } from "@/types-db";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -28,16 +35,29 @@ interface ProductFormProps {
   initialData: Product;
   categories: Categories[];
   sizes: Sizes[];
-  kitchen: Kitchen[];
+  kitchens: Kitchen[];
   cuisines: Cuisines[];
 }
 
 const formSchema = z.object({
   name: z.string().min(1),
-  value: z.string().min(1),
+  kitchen: z.string().min(1),
+  cuisine: z.string().min(1),
+  size: z.string().min(1),
+  category: z.string().min(1),
+  price: z.coerce.number().min(1),
+  images: z.object({ url: z.string() }).array(),
+  isFeatured: z.boolean().default(false).optional(),
+  isArchived: z.boolean().default(false).optional(),
 });
 
-const ProductForm = ({ initialData }: ProductFormProps) => {
+const ProductForm = ({
+  initialData,
+  categories,
+  sizes,
+  kitchens,
+  cuisines,
+}: ProductFormProps) => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData,
@@ -111,6 +131,7 @@ const ProductForm = ({ initialData }: ProductFormProps) => {
           className="w-full space-y-8"
         >
           <div className="grid grid-cols-3 gap-8">
+            {/* Product Name */}
             <FormField
               control={form.control}
               name="name"
@@ -128,20 +149,138 @@ const ProductForm = ({ initialData }: ProductFormProps) => {
                 </FormItem>
               )}
             />
+            {/* Price */}
             <FormField
               control={form.control}
-              name="value"
+              name="price"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Value</FormLabel>
+                  <FormLabel>Price</FormLabel>
                   <FormControl>
                     <Input
+                      type="number"
                       disabled={isLoading}
-                      placeholder="Product"
+                      placeholder="0"
                       {...field}
                     />
                   </FormControl>
                   <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Category */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Category</FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Categories" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {categories.map((category) => (
+                          <SelectItem key={category.id} value={category.id}>
+                            {category.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            {/* Kitchen */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Kitchen</FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Kitchens" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {kitchens.map((kitchen) => (
+                          <SelectItem key={kitchen.id} value={kitchen.id}>
+                            {kitchen.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            {/* Cuisine */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Cuisine</FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Cuisines" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {cuisines.map((cuisine) => (
+                          <SelectItem key={cuisine.id} value={cuisine.id}>
+                            {cuisine.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            {/* Size */}
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Size</FormLabel>
+                  <FormControl>
+                    <Select
+                      disabled={isLoading}
+                      onValueChange={field.onChange}
+                      value={field.value}
+                      defaultValue={field.value}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Size" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {sizes.map((size) => (
+                          <SelectItem key={size.id} value={size.id}>
+                            {size.name}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
                 </FormItem>
               )}
             />
