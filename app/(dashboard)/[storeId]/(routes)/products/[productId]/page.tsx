a0d5af889adff778840a979906e7ace110da19a3
Rorisang Putu <rorisangputu@gmail.com>
2024-10-26 19:59:54 +0200

    fetching prop data

--- app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/[productId]/page.tsx
@@ -1,6 +1,6 @@
 import { db } from "@/lib/firebase";
-import { Product } from "@/types-db";
-import { doc, getDoc } from "firebase/firestore";
+import { Categories, Cuisines, Kitchen, Product, Sizes } from "@/types-db";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import ProductForm from "./components/ProductForm";
 
 const ProductPage = async ({
@@ -14,10 +14,29 @@ const ProductPage = async ({
     )
   ).data() as Product;
 
+  const categories = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "categories"))
+  ).docs.map((doc) => doc.data()) as Categories[];
+  const kitchens = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "kitchens"))
+  ).docs.map((doc) => doc.data()) as Kitchen[];
+  const sizes = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "sizes"))
+  ).docs.map((doc) => doc.data()) as Sizes[];
+  const cuisines = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "cuisines"))
+  ).docs.map((doc) => doc.data()) as Cuisines[];
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <ProductForm initialData={product} />
+        <ProductForm
+          initialData={product}
+          categories={categories}
+          kitchens={kitchens}
+          sizes={sizes}
+          cuisines={cuisines}
+        />
       </div>
     </div>
   );
