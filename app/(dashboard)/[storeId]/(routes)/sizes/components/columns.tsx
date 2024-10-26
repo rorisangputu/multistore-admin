e2f2c92e879aaafdaa03831be8606ce71346e8ab
Rorisang Putu <rorisangputu@gmail.com>
2024-10-26 14:09:45 +0200

    sizes column

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
+import CellAction from "./CellAction";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type SizeColumns = {
+  id: string;
+  billboardLabel: string;
+  name: string;
+  createdAt: string;
+};
+
+export const Columns: ColumnDef<SizeColumns>[] = [
+  {
+    accessorKey: "name",
+    header: "Category",
+  },
+  {
+    accessorKey: "billboardLabel",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Billboard Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    header: "Actions",
+    id: "Actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
