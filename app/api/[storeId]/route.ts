5e98b26725c62d20d60cdfcb15739d54545c70a2
Rorisang Putu <rorisangputu@gmail.com>
2024-10-27 21:17:01 +0200

    store delete func

--- app/api/[storeId]/route.ts
+++ app/api/[storeId]/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server"
 import { auth } from '@clerk/nextjs/server'
-import { addDoc, collection, deleteDoc, doc, getDoc, serverTimestamp, updateDoc } from "firebase/firestore"
-import { db } from "@/lib/firebase"
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, serverTimestamp, updateDoc } from "firebase/firestore"
+import { db, storage } from "@/lib/firebase"
 import { Store } from "@/types-db"
+import { deleteObject, ref } from "firebase/storage"
 
 
 export const PATCH = async (req : Request, {params}: {params:{storeId: string}}) => {
@@ -52,6 +53,106 @@ export const DELETE = async (req : Request, {params}: {params:{storeId: string}}
 
 
         const docRef = doc(db, "stores", params.storeId);
+
+        //TODO : Delete all the subcollections along with associated data
+
+        //Billboards and its images
+        const billboardsQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/billboards`)
+        );
+
+        billboardsQuerySnapshot.forEach(async (billboardDoc) => {
+            await deleteDoc(billboardDoc.ref)
+
+            //remove the images from storage
+            const imageUrl = billboardDoc.data().imageUrl
+            if (imageUrl) {
+                const imageRef = ref(storage, imageUrl)
+                await deleteObject(imageRef)
+            }
+        })
+
+        // Categories 
+
+        const categoriesQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/categories`)
+        );
+        categoriesQuerySnapshot.forEach(async (categoryDoc) => {
+            await deleteDoc(categoryDoc.ref)
+        });
+
+        //Sizes
+
+        const sizesQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/sizes`)
+        );
+        sizesQuerySnapshot.forEach(async (sizeDoc) => {
+            await deleteDoc(sizeDoc.ref)
+        });
+
+        //Kitchens
+        const kitchenQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/kitchens`)
+        );
+        kitchenQuerySnapshot.forEach(async (kitchenDoc) => {
+            await deleteDoc(kitchenDoc.ref)
+        });
+
+        //Cuisines
+        const cuisineQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/cuisines`)
+        );
+        cuisineQuerySnapshot.forEach(async (cuisineDoc) => {
+            await deleteDoc(cuisineDoc.ref)
+        });
+
+        //Products
+        const productsQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/products`)
+        );
+
+        productsQuerySnapshot.forEach(async (productDoc) => {
+            await deleteDoc(productDoc.ref);
+
+            //remove the images from the storage
+            const imagesArray = productDoc.data().images;
+            if (imagesArray && Array.isArray(imagesArray)) {
+                await Promise.all(
+                    imagesArray.map(async (image) => {
+                        const imageRef = ref(storage, image.url);
+                        await deleteObject(imageRef);
+                    })
+                );
+            }
+        });
+
+        //Orders 9:56
+        const ordersQuerySnapshot = await getDocs(
+            collection(db, `stores/${params.storeId}/orders`)
+        );
+
+        ordersQuerySnapshot.forEach(async (orderDoc) => {
+            await deleteDoc(orderDoc.ref)
+
+            const ordersItemArray = orderDoc.data().orderItems;
+            if (ordersItemArray && Array.isArray(ordersItemArray)) {
+                await Promise.all(
+                    ordersItemArray.map(async (orderItem) => {
+                        const itemImagesArray = orderItem.images;
+                        if (itemImagesArray && Array.isArray(itemImagesArray)) {
+                            await Promise.all(
+                                itemImagesArray.map(async (image) => {
+                                    const imageRef = ref(storage, image.url);
+                                    await deleteObject(imageRef);
+                                })
+                            );
+                        }
+                    })
+                );
+            }
+        });
+
+
         await deleteDoc(docRef);
 
         return NextResponse.json({msg: "Store and its subcollections deleted"});
